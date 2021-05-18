f6a47912a72124a5ec62699d949bcd9ace907835
muneeb <thinkmuneeb@gmail.com>
2021-05-18 10:28:10 +0500

    simple list view works

--- app/src/main/java/com/example/wowapp/MainActivity.java
+++ app/src/main/java/com/example/wowapp/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     Button numBtn;
     TextView numTxt;
 
+    ListView listView;
+
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WindowsMobile","Blackberry","WindowsMobile","Blackberry",
+            "WebOS","Ubuntu","Windows7","Max OS X","Windows7","Max OS X","Windows7","Max OS X"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,20 +30,11 @@ public class MainActivity extends AppCompatActivity {
         numBtn = (Button) findViewById(R.id.numBtn);
         numTxt = (TextView) findViewById(R.id.numTxt);
 
+        listView = (ListView) findViewById(R.id.listView);
 
-        numBtn.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        // update state
-                        i++;
-
-                        // update view
-                        numTxt.setText(i+"");
-                    }
-                }
-        );
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,mobileArray);
+        listView.setAdapter(adapter);
+        numBtn.setOnClickListener(getClickListener());
     }
 
     public void onSaveInstanceState(Bundle savedInstanceState){
@@ -66,5 +64,16 @@ public class MainActivity extends AppCompatActivity {
         catch(Exception ex){
             Log.i(u.TAG,"ex i onRestoreInstanceState: " + ex.getMessage());
         }
+    }
+
+    private View.OnClickListener getClickListener(){
+        return (View.OnClickListener) v -> {
+
+            // update state
+            i++;
+
+            // update view
+            numTxt.setText(i+"");
+        };
     }
 }
